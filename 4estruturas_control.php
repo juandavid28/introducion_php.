cf900ca315ac67fd5be585a44c5f5d08b46c0f54
ivanrueda1 <ivanrueda@colegioguanenta.edu.co>
2023-07-18 09:37:53 -0500

    Estrutura de control

--- 4estruturas_control.php
+++ 4estruturas_control.php
@@ -8,30 +8,90 @@
 <body>
     <?php
     //if, if.else
+    echo "<h1>if</h1>";
+    //Ejemplo 1
+    $color = "rojo";
+    if($color = "rojo")
+    {
+        print("Efectivamente, el color es rojo");
+    }
+
+    echo "<br>";
+
+    //Ejemplo 2
+    $x = 10;
+    $y = 15;
+    if($x == $y)
+    {
+        print("x e y son iguales");
+    }
+    elseif($x > $y)
+    {
+        print("x es mayor que y");
+    }
+    elseif($x < $y)
+    {
+        print("x es mayor que y");
+    }
+
+    echo "<br>";
+    echo "<h1>while</h1>";
     //while
+    // Indica qeu mientra no se cumpla una determinada condicion, no se saldra del bucle y no
+    // Saltara a la siguiente linea de codigo
+    $x = 10;
+    while(--$x)
+    {
+        echo "<big>";
+        echo "<b>";
+        print("Numero: ".$x);
+        echo "<br>";
+        echo "<hr>";
+
+    }
     //do.while
+
     //for
+    //sera utilizado para ejecutar un bucle eterminado numero de veces, hasta que se cumpla una
+    // condicion
+    echo "<br>";
+    echo "<h1>for</h1>";
+    for($x=5; $x<=10; $x++)
+    {
+        print("Numero: ".$x."<br>");
+    }
     //require
+    //Sirve para incluir archivos en nuestras paginas y solo sera necesario hacer referencia
+    //a este archivo con la instrucion requiere. se usa principalmente para definir variables, 
+    //y estas estaran listas una vez hagamos una llamada al archivo donde esten guardadas.
+
     //include
+    // El funcionamiento es igual que el de la instruccion requiere(), con la diferencia de que si
+    // puede procesar el codigo tanto veces como la llamemos a esa pagina externa.
+    echo "<br>";
+    echo "<h1>Include</h1>";
+    include("variables.php");
+    echo "<br>";
+    print("$x", "$z", "$y");
+
     //swit
+    // Se utiliza para comprobar un dato entre varias pasibilidades
+    echo "<br>";
+    echo "<h1>swite</h1>";
+    $color = "negro";
+    switch($color)
+    {
+        case "blanco";
+           $sector = "claro";
+           break;
+        case "naranja";
+           $sector = "normal";
+           break;
+        case "negor";
+           $sector = "oscuro";
+           break;
+    }
+    print($sector);
     ?>
 </body>
 </html>
-
-//Operadores de union (concatenacion) de cadenas
-    // Para la union de cadenas se emplea el pnto (.)
-    echo "<h1>Operadores de union de cadenas</h1><br>"
-    $t = "Ejemplo";
-    $w = 'union';
-    $x = "de";
-    $y = "cadenas";
-    $z = ' ';
-    $resultado1 = $t;
-    $resultado2 = $t.$z.$x.$z.$w.$z.$x.$z.$y;
-    echo '<b><h1>';
-    echo $resultado1;
-    echo "<hr>";
-    echo $resultado2;
-    echo "</b></hr>";
-
-    ?>
